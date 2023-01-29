5f91ea107a008aa9ffafb8d883750ad1a889f1db
Magda Muhammad <sonaz2012@gmail.com>
2023-01-29 20:00:44 +0200

    edit router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+//import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+
 import Footer from './components/UI/Footer';
 import NewProduct from './components/NewProduct/NewProduct';
 import ProductList from "./components/ProductList/ProductList";
 
 
-const router = createBrowserRouter([
-    {path: '/', element: <ProductList />},
-    {path: '/add-product', element: <NewProduct />}
-]);
+// const router = createBrowserRouter([
+//     {path: '/', element: <ProductList />},
+//     {path: '/add-product', element: <NewProduct />}
+// ]);
 
 function App() {
 
   return (
     <>
-      <RouterProvider router={router} />
+      <Router>
+          <Switch>
+              <Route path='/products_assignment' exact component={ProductList}/>
+              <Route path='/products_assignment/add-product' exact component={NewProduct}/>
+          </Switch>
+      </Router>
+
+      {/* <RouterProvider router={router} /> */}
       <Footer />
     </>
   );
